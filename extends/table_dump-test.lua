98b197b39ad67e38b9e7604fe379a99dd3d9f761
wuyuhong <wuyuhong@ihuman.com>
2022-08-05 11:16:29 +0800

    change crlf to lf

--- extends/table_dump-test.lua
+++
@@ -1,3 +0,0 @@
-require "table_dump"
-
-print("Hello World!")
--- test-case.lua
+++
@@ -1,8 +0,0 @@
-local case_name = select(1, ...)
-if not case_name then
-    error("case_name is nil")
-end
-print("test-case: " .. case_name)
-
-require "extends.table_dump-test"
-
