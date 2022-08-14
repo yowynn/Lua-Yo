f338b622bc0e4d7fdd5a9c019aaf5a0bc4e60840
wynn <yowangyuxi@outlook.com>
2022-08-14 23:11:33 +0800

    new luarocks path

--- build-luarocks.cmd
+++ build-luarocks.cmd
@@ -3,21 +3,36 @@
 :: file build-luarocks.cmd
 :: ========================
 setlocal
-:: you may change the following variable's value
-:: to suit the downloaded version
-set luarocks_version=3.9.1
+:: set your download luarocks version
+set LUAROCKS_VERSION=3.9.1
+:: set lue version luarocks uses
+set LUA_VERSION=5.4
+
 set work_dir=%~dp0
 :: Removes trailing backslash
-:: to enhance readability in the following steps
 set work_dir=%work_dir:~0,-1%
-
-
-set luarocks_install_dir=%work_dir%\lua\luarocks
-set luarocks_build_dir=%work_dir%\luarocks-%luarocks_version%
-echo %luarocks_build_dir%
+set source_dir=%work_dir%\luarocks-%LUAROCKS_VERSION%
+set target_dir=%work_dir%\env
 :: just copy the downloaded files to the build directory
-mkdir %luarocks_install_dir%
-copy "%luarocks_build_dir%\luarocks.exe" "%luarocks_install_dir%\luarocks.exe"
-copy "%luarocks_build_dir%\luarocks-admin.exe" "%luarocks_install_dir%\luarocks-admin.exe"
-
+echo **** COPY TO TARGET PATH ****
+mkdir %target_dir%\bin
+copy %source_dir%\luarocks.exe %target_dir%\bin\*.*
+copy %source_dir%\luarocks-admin.exe %target_dir%\bin\*.*
+echo **** SUCCESSFUL ****
+echo.
+:: test lua
+echo **** LUAROCKS TEST ****
+mkdir %target_dir%\cfg
+set luarocks_config=%target_dir%\cfg\luarocks.lua
+if not exist %luarocks_config% (type nul>%luarocks_config%)
+%target_dir%\bin\luarocks.exe config home_tree %target_dir%
+%target_dir%\bin\luarocks.exe config rocks_trees[1] %target_dir%
+%target_dir%\bin\luarocks.exe config lua_interpreter lua.exe
+%target_dir%\bin\luarocks.exe config lua_version %LUA_VERSION%
+%target_dir%\bin\luarocks.exe config variables.LUA_DIR %target_dir%
+%target_dir%\bin\luarocks.exe config variables.LUA_BINDIR %target_dir%\bin
+%target_dir%\bin\luarocks.exe config variables.LUA_INCDIR %target_dir%\include
+%target_dir%\bin\luarocks.exe config variables.LUA_LIBDIR %target_dir%\bin
+%target_dir%\bin\luarocks.exe
+echo.
 pause
---
+++ env/cfg/luarocks.lua
@@ -0,0 +1,12 @@
+home_tree = "P:\\Github\\Lua-Yo\\env"
+lua_interpreter = "lua.exe"
+lua_version = "5.4"
+rocks_trees = {
+   "P:\\Github\\Lua-Yo\\env"
+}
+variables = {
+   LUA_BINDIR = "P:\\Github\\Lua-Yo\\env\\bin",
+   LUA_DIR = "P:\\Github\\Lua-Yo\\env",
+   LUA_INCDIR = "P:\\Github\\Lua-Yo\\env\\include",
+   LUA_LIBDIR = "P:\\Github\\Lua-Yo\\env\\bin"
+}
