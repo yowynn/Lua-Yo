7cab7eba5a842fbd178071c30bf93d8787e8eb95
wynn <yowangyuxi@outlook.com>
2022-08-14 20:23:03 +0800

    new lua bin path

--- build-lua.cmd
+++ build-lua.cmd
@@ -10,12 +10,11 @@ set work_dir=%~dp0
 :: Removes trailing backslash
 :: to enhance readability in the following steps
 set work_dir=%work_dir:~0,-1%
-set lua_install_dir=%work_dir%\lua
-set compiler_bin_dir=%work_dir%\tdm-gcc\bin
+set lua_install_dir=%work_dir%\env
 set lua_build_dir=%work_dir%\lua-%lua_version%
-set path=%compiler_bin_dir%;%path%
 cd /D %lua_build_dir%
-mingw32-make PLAT=mingw
+:: the main build command
+make PLAT=mingw
 echo.
 echo **** COMPILATION TERMINATED ****
 echo.
@@ -36,6 +35,8 @@ copy %lua_build_dir%\src\lualib.h %lua_install_dir%\include\*.*
 copy %lua_build_dir%\src\lauxlib.h %lua_install_dir%\include\*.*
 copy %lua_build_dir%\src\lua.hpp %lua_install_dir%\include\*.*
 copy %lua_build_dir%\src\liblua.a %lua_install_dir%\lib\liblua.a
+:: optional: clean up
+make clean
 echo.
 echo **** BINARY DISTRIBUTION BUILT ****
 echo.
