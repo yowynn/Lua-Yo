cc0c791d0400f3ad8d7f00e0c84b70e216850069
Wynne <yowangyuxi@outlook.com>
2023-05-06 01:20:51 +0800

    table serialization

--- test-cases/table.lua
+++ test-cases/table.lua
@@ -1,7 +1,22 @@
 print("Test: table module")
 
 require("table_extend")
+require("table_serialization")
 
-for k, v in pairs(table) do
-    print(k, v)
-end
+
+case("serialization", function(t)
+    print("dump:")
+    print(table.dump(t))
+    print("serialize:")
+    local s = table.serialize(t)
+    print(s)
+    print("dump deserialize:")
+    print(table.dump(table.deserialize(s)))
+end)
+
+local t = {1,2,aa="hah"}
+local t2 = {t = t}
+t.t2 = t2
+t[t2] = {}
+t[print] = {[math.huge] = 666}
+test("serialization", t)
